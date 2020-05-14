6ec474041af224a6452e96163316e11af10c21b7
vinicius henrard gasparotto <vinigasparotto@gmail.com>
2020-05-13 22:02:43 -0300

    first commit

---
+++ src/main/java/br/edu/up/vidasustentavel/VidaSustentavelApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.up.vidasustentavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidaSustentavelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidaSustentavelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/up/vidasustentavel/VidaSustentavelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.up.vidasustentavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaSustentavelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
